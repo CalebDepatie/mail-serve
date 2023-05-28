ea47e7c68501faf78ff01b0de96de191b0512511
Caleb Depatie <caleb.depatie@outlook.com>
2023-05-27 23:41:34 -0400

    Updated go-common

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/CalebDepatie/go-docker
 go 1.16
 
 require (
-	github.com/CalebDepatie/go-common v0.0.0-20230314173403-7f7ab99cd302
-	github.com/joho/godotenv v1.4.0
+	github.com/CalebDepatie/go-common v0.0.0-20230528033307-b505cf112375 // indirect
+	github.com/joho/godotenv v1.5.1
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-  "errors"
+	"errors"
 	c "github.com/CalebDepatie/go-common"
 	"github.com/joho/godotenv"
 	"gopkg.in/gomail.v2"
@@ -19,8 +18,15 @@ func main() {
 		c.LogFatal("Error loading .env file")
 	}
 
-  connectToGhidorah()
-  
+	reg := c.GhidorahReg{
+		Name:             "mail",
+		ExternAccessible: false,
+		Internal:         true,
+		Port:             "10000",
+	}
+
+	c.ConnectToGhidorah(reg, os.Getenv("GHIDORAH"))
+
 	http.HandleFunc("/send", sendMail)
 
 	c.LogFatal(http.ListenAndServe(":10000", nil))
@@ -65,42 +71,4 @@ func sendMail(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 
-}
-
-// initialize connection to Ghidorah
-func connectToGhidorah() {
-	reg := struct {
-		Name             string `json:name`
-		ExternAccessible bool   `json:extern_facing`
-		Internal         bool   `json:internal`
-		Port             string `json:port`
-	}{
-		Name:             "mail",
-		ExternAccessible: false,
-		Internal:         true,
-		Port:             "10000",
-	}
-
-	var buf bytes.Buffer
-	err := json.NewEncoder(&buf).Encode(reg)
-	if err != nil {
-		c.LogFatal("Could not encode Registration Data", err)
-	}
-
-	req, err := http.NewRequest(http.MethodGet, os.Getenv("GHIDORAH")+"/register", &buf)
-	if err != nil {
-		c.LogFatal("Could not create service registation request", err)
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		c.LogFatal("Could not connect to Ghidorah", err)
-	}
-
-	if resp.StatusCode != 200 {
-		c.LogFatal("Could not register service", resp.Status)
-	}
-
-	c.LogInfo("Connected to Ghidorah")
 }
